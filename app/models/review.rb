65c68dc19d9f34cd6a1172525978faef0c1b2530
watarun <watarun54@gmail.com>
2020-01-31 00:50:01 +0900

    add validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :lecture
 
+  validates :good_point, presence: true, length: { maximum: 400 }
+  validates :bad_point, presence: true, length: { maximum: 400 }
+
   scope :newest, -> { order(created_at: :desc) }
 end
