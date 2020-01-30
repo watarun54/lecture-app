79082f11fe2c9b41eab378f708e8d5aefb32fb0f
watarun <watarun54@gmail.com>
2020-01-30 23:57:30 +0900

    feature ranking of lectures

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,8 +1,12 @@
 class LecturesController < ApplicationController
   before_action :set_lecture, only: [:show]
 
+  include ReviewsHelper
+
   def index
     @lectures = Lecture.all.page(params[:page]).per(LECTURES_PER)
+
+    @lectures_by_rate, @lectures_by_reviews = rank_data
   end
 
   def show
@@ -27,5 +31,15 @@ class LecturesController < ApplicationController
 
   def set_lecture
     @lecture = Lecture.find(params[:id])
+  end
+
+  def rank_data
+    d = Lecture.all.includes(:reviews).map do |lec|
+      next if lec.reviews.empty?
+      [lec, average_rate(lec.reviews), lec.reviews.size]
+    end.compact
+    lectures_by_rate = d.sort_by{|x| -x[1]}
+    lectures_by_reviews = d.sort_by{|x| -x[2]}
+    return lectures_by_rate, lectures_by_reviews
   end
 end
